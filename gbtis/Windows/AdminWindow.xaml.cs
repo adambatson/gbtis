df9649ff5dc26e82a426ac9ae673e5f08a298429
Richard Carson <rscarson@rogers.com>
2017-01-31 15:19:54 -0500

    Neuter adminwindow

--- gbtis/Windows/AdminWindow.xaml.cs
+++ gbtis/Windows/AdminWindow.xaml.cs
@@ -11,7 +11,6 @@ namespace gbtis.Windows {
         // Window events
         public event EventHandler Exit;
         public event EventHandler Standby;
-        public event EventHandler Input;
 
         /// <summary>
         /// Initialize the window
@@ -34,14 +33,6 @@ namespace gbtis.Windows {
         }
 
         /// <summary>
-        /// Set the status bar's text
-        /// </summary>
-        /// <param name="text"></param>
-        public void SetStatus(String text) {
-            statusText.Text = text;
-        }
-
-        /// <summary>
         /// Update the frame in the video feed
         /// </summary>
         /// <param name="frame">The new frame</param>
@@ -59,35 +50,12 @@ namespace gbtis.Windows {
         }
 
         /// <summary>
-        /// Display the about box
-        /// </summary>
-        /// <param name="sender">Event source</param>
-        /// <param name="args">Event args</param>
-        private void HelpAbout_Click(object sender, EventArgs args) {
-            MessageBox.Show(
-                Properties.Resources.aboutText,
-                Properties.Resources.aboutTitle,
-                MessageBoxButton.OK,
-                MessageBoxImage.Information
-            );
-        }
-
-        /// <summary>
         /// Return to standby mode
         /// </summary>
         /// <param name="sender">Event source</param>
         /// <param name="e">Event args</param>
         private void FileStandby_Click(object sender, EventArgs args) {
             Standby?.Invoke(this, args);
-        }
-
-        /// <summary>
-        /// Start the input mode
-        /// </summary>
-        /// <param name="sender">Event source</param>
-        /// <param name="e">Event args</param>
-        private void FileInput_Click(object sender, EventArgs args) {
-            Input?.Invoke(this, args);
         }
     }
 }

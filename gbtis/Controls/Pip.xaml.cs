081be2dcc4a5fb3e0c75ca9fea9dafe6d2002ba7
Richard Carson <rscarson@rogers.com>
2017-02-03 15:06:58 -0500

    Added wiring for body tracker pips

---
+++ gbtis/Controls/Pip.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gbtis.Controls {
+    /// <summary>
+    /// Interaction logic for Pip.xaml
+    /// </summary>
+    public partial class Pip : UserControl {
+        /// <summary>
+        /// Set the pip as an active pip
+        /// </summary>
+        public bool Active {
+            set {
+                Background = new SolidColorBrush(value ? Colors.Red : Colors.Blue);
+        } }
+
+        /// <summary>
+        /// Set the location of the pip
+        /// </summary>
+        public Point Position {
+            set {
+                Margin = new Thickness(value.X - Width/2, value.Y - Height/2, 0, 0);
+        } }
+
+        /// <summary>
+        /// Register the Text property of the control
+        /// </summary>
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(Pip), new PropertyMetadata("#"));
+        public string Text {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        /// <summary>
+        /// Register the Color property of the control
+        /// </summary>
+        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
+            "Color", typeof(Color), typeof(Pip), new PropertyMetadata(Colors.Blue));
+        public Color Color {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        public Pip() {
+            InitializeComponent();
+        }
+
+        public Pip(String label) : this() {
+            Text = label;
+        }
+    }
+}
--- gbtis/Windows/AdminWindow.xaml.cs
+++ gbtis/Windows/AdminWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using gbtis.Controls;
+using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace gbtis.Windows {
@@ -7,6 +9,7 @@ namespace gbtis.Windows {
     /// Interaction logic for AdminWindow.xaml
     /// </summary>
     public partial class AdminWindow : Window {
+        private Kinect kinect;
 
         // Window events
         public event EventHandler Exit;
@@ -19,17 +22,43 @@ namespace gbtis.Windows {
             DataContext = this;
             InitializeComponent();
 
-            Kinect kinect = Kinect.getInstance();
-            kinect.BitMapReady += BitMapArrived;
+            kinect = Kinect.getInstance();
+            kinect.BitMapReady += Kinect_BitMapArrived;
+            kinect.SensorStatusChanged += Kinect_SensorStatusChanged;
+        }
+
+        private void Kinect_SensorStatusChanged(bool isAvailable) {
+            this.Dispatcher.Invoke(new Action(() =>
+                noKinect.Visibility = isAvailable ? Visibility.Visible : Visibility.Hidden));
         }
 
         /// <summary>
         /// Update the camera feed from the sensor
         /// </summary>
         /// <param name="img">The latest ImageSource</param>
-        void BitMapArrived(ImageSource img) {
-            this.Dispatcher.Invoke(new Action(() =>
-                sensorFeed.Source = img));
+        void Kinect_BitMapArrived(ImageSource img) {
+            this.Dispatcher.Invoke(new Action(() => {
+                sensorFeed.Source = img;
+                noKinect.Visibility = Visibility.Visible;
+
+                // Update pips
+                pipContainer.Children.Clear();
+                usersMenu.Items.Clear();
+                ////// Foreach body
+                Pip pip = new Controls.Pip(1.ToString()); //tag
+                pip.Position = kinect.ScaleToSize(new Point(), new Size(img.Width, img.Height), new Size(this.ActualWidth, this.ActualHeight)); // Head location
+                pip.Active = true; // If is active body
+                pipContainer.Children.Add(pip);
+
+                MenuItem item = new MenuItem();
+                item.Header = 1.ToString(); //tag
+                item.Tag = 1; //tag
+                item.Click += (s,e) => { /* kinect.ActiveBody = (int)((MenuItem)s).Tag;*/ };
+                usersMenu.Items.Add(item);
+                //////
+
+                usersMenu.IsEnabled = usersMenu.Items.Count > 0;
+            }));
         }
 
         /// <summary>
